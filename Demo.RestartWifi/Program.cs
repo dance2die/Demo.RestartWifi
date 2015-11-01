b2ae129ccaa9caf8346a56d064e69df91dc74dd0
dance2die <dance2die@hotmail.com>
2015-11-01 09:27:20 -0500

    Skeletal code

---
+++ Demo.RestartWifi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.RestartWifi
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Restart Network Adapter - Call Restart-NetAdapter PowerShell command
+
+			// Connect WiFi - Use SimpleWifi
+		}
+	}
+}
